8c862ac7a2acc633c89aea3634b28b3f2d485f25
Riguidix Rodriguez <reydescelrodriguez@gmail.com>
2021-01-07 19:47:37 -0700

    JavaScript Best Practices #3 Challenge Completed

--- javascript/best-practices/vendingMachine.js
+++ javascript/best-practices/vendingMachine.js
@@ -3,26 +3,10 @@ var balanceManager = require('./balanceManager');
 var changeHandler = require('./changeHandler');
 var productInventory = require('./productInventory');
 
-var products = [
-  {
-    name: 'Skittles',
-    price: 85,
-    id: 'A1'
-  }
-];
-
 module.exports = {
-  getProducts: function() { 
-    return products;
-  },
-
-  getProduct: function(productId) {
-    var product = products.find(function(p) { return p.id === productId; });
-    return product;
-  },
-
   insertCoin: function(coinType){
     var value = changeHandler.getAmount(coinType);
+    
     balanceManager.increaseBalance(value);
   },
 
@@ -36,13 +20,17 @@ module.exports = {
 
   releaseChange: function(){
     var currentBalance = balanceManager.getBalance();
+
     balanceManager.decreaseBalance(currentBalance);
+
     return this.convertToChange(currentBalance);
   },
 
   vendProduct: function(productId){
-    var product = this.getProduct(productId);
+    var product = productInventory.getProduct(productId);
+
     balanceManager.decreaseBalance(product.price);
+
     return product;
   }
 
