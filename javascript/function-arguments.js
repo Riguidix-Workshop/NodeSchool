04bba437119e466ea8437c6a6c5fe5a18354044e
Riguidix Rodriguez <reydescelrodriguez@gmail.com>
2021-01-07 18:25:35 -0700

    JavaScripting #19 Challenge Completed

---
+++ javascript/function-arguments.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Number} z 
+ * @returns {Number} The product of the y and z (params) in addition to the x (param)
+ */
+function math (x, y, z) {
+    return x + (y * z);
+}
+
+console.log(math(53, 61, 67));
