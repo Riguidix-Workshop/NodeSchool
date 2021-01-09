d97aef8535af77f17ffa1e3c8fc35d5dbe35a3f4
Riguidix Rodriguez <reydescelrodriguez@gmail.com>
2021-01-08 18:13:33 -0700

    JavaScript Promise It Won't Hurt #6 Fixing Challenge

--- javascript/promises/promise-es6.js
+++ javascript/promises/promise-es6.js
@@ -1,18 +1,23 @@
 // Challenge from: https://www.codingame.com/playgrounds/347/javascript-promises-mastering-the-asynchronous/more-pratice-with-promises
 function job(data) {
     var promise = new Promise((resolve, reject) => {
-        if (isNaN(data)) {
-            reject('error');
-        } else if (data % 2 == 0) { // Even
-            setTimeout(() => {
-                reject('even');
-            }, 2000);
-        } else { // Odd
-            setTimeout(() => {
-                resolve('odd');
-            }, 1000);            
-        }
+        if (isNaN(data)) reject('error');
     });
+
+    promise.catch((error) => console.log(error));
+
+    /**
+     * Validating the Number methods
+     */
+    if (data % 2 == 0) { // Even        
+        promise = Promise.reject('even');
+    } else { // Odd
+        promise = Promise.reject('odd');
+    }
 
     return promise;
 }
+
+job('string');
+job(2);
+job(3);
