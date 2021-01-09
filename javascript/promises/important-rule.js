e5b071544fcba2ec037a9ebe58a7e1ab6410aece
Riguidix Rodriguez <reydescelrodriguez@gmail.com>
2021-01-08 19:41:15 -0700

    JavaScript Promise It Won't Hurt #10 Challenge Completed

---
+++ javascript/promises/important-rule.js
@@ -0,0 +1,23 @@
+function alwaysThrows () {
+   throw new Error("OH NOES");
+}
+
+function iterate (number) {
+    console.log(number);
+
+    return number + 1;
+}
+
+Promise
+    .resolve(iterate(1))
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(alwaysThrows)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .then(iterate)
+    .catch((error) => console.log(error.message));
